092182dcb1b4c02e61434a0d354fe9c3ee7e7787
Daniel <dmoody256@gmail.com>
2019-02-18 05:30:42 -0600

    added support for setting build dir and progress counter

--- SconsUtils.py
+++ SconsUtils.py
@@ -36,6 +36,40 @@ from SCons.Errors import BuildError
 
 
 from BuildUtils.ColorPrinter import ColorPrinter
+from BuildUtils import get_num_cpus
+
+
+def SetBuildJobs(env):
+    ###################################################
+    # Determine number of Jobs
+    # start by assuming num_jobs was not set
+    NUM_JOBS_SET = False
+    if GetOption("num_jobs") == 1:
+        # if num_jobs is the default we need to check sys.argv
+        # to see if the user happened to set the default
+        for arg in sys.argv:
+            if arg.startswith("-j") or arg.startswith("--jobs"):
+                if arg == "-j" or arg == "--jobs":
+                    if int(sys.argv[sys.argv.index(arg)+1]) == 1:
+                        NUM_JOBS_SET = True
+                else:
+                    if arg.startswith("-j"):
+                        if int(arg[2:]) == 1:
+                            NUM_JOBS_SET = True
+    else:
+        # user must have set something if it wasn't default
+        NUM_JOBS_SET = True
+
+    # num_jobs wasn't specificed so let use the
+    # max number since the user doesn't seem to care
+    if not NUM_JOBS_SET:
+        NUM_CPUS = get_num_cpus()
+        ColorPrinter().InfoPrint(" Building with " + str(NUM_CPUS) + " parallel jobs")
+        env.SetOption("num_jobs", NUM_CPUS)
+    else:
+        # user wants a certain number of jobs so do that
+        ColorPrinter().InfoPrint(
+            " Building with " + str(GetOption('num_jobs')) + " parallel jobs")
 
 
 def ImportVar(import_name):
@@ -164,6 +198,7 @@ class ProgressCounter(object):
             self.count = 0.0
             self.progress_sources = dict()
             self.target = None
+            self.target_install = ''
             for source in sources:
                 # print("Making key: " + os.path.splitext(source)[0])
                 self.progress_sources[os.path.splitext(source)[0]] = False
@@ -172,29 +207,27 @@ class ProgressCounter(object):
     def __init__(self):
         self.printer = ColorPrinter()
         self.progress_builders = []
-        self.target_name_size = 0
 
     def AddBuild(self, env, sources, target):
-        if self.target_name_size < len(target):
-            self.target_name_size = len(target)
-            # self.printer.SetSize(self.target_name_size)
-        pathed_sources = [env.File(source).abspath.replace('\\', '/').replace(env['PROJECT_DIR'] + '/', '')
-                          for source in sources]
+        env['PROJECT_DIR'] = env.get(
+            'PROJECT_DIR', env.Dir('.').abspath)
+        # self.printer.SetSize(self.target_name_size)
+        # pathed_sources = [env.File(source).abspath.replace('\\', '/').replace(env['PROJECT_DIR'] + '/', '')
+        #                  for source in sources]
         self.progress_builders.append(
-            self.ProgressBuild(pathed_sources, target))
+            self.ProgressBuild(sources, target))
 
     def __call__(self, node, *args, **kw):
         # print(str(node))
 
         slashed_node = str(node).replace("\\", "/")
         for build in self.progress_builders:
-            # print(build.target + ": "+str(node.get_state())+" - " + slashed_node)
-            if(slashed_node.endswith("build/" + build.target)):
+            #print(build.target + ": "+str(node.get_state())+" - " + slashed_node)
+            if(slashed_node.endswith(build.target)):
 
                 if(build.count == 0):
                     self.printer.InfoPrint(
                         self.printer.OKBLUE + "[ " + os.path.splitext(build.target)[0] + " ]" + self.printer.ENDC + " Building " + build.target)
-
                 filename = os.path.basename(slashed_node)
                 if(node.get_state() == 2):
                     self.printer.LinkPrint(os.path.splitext(
@@ -209,7 +242,7 @@ class ProgressCounter(object):
            or slashed_node.endswith(".os")):
 
             slashed_node_file = os.path.splitext(slashed_node)[0]
-            # print(" - " + slashed_node_file)
+            #print(" - " + slashed_node_file)
             for build in self.progress_builders:
                 try:
                     if(not build.progress_sources[slashed_node_file]):
@@ -235,9 +268,11 @@ class ProgressCounter(object):
                     pass
 
 
-def SetupBuildEnv(env, prog_type, prog_name, source_files, install_dir):
+def SetupBuildEnv(env, progress, prog_type, prog_name, source_files, build_dir, install_dir):
 
     build_env = env.Clone()
+    build_env['PROJECT_DIR'] = build_env.get(
+        'PROJECT_DIR', build_env.Dir('.').abspath)
 
     header_files = []
     if prog_type == "unit":
@@ -254,22 +289,31 @@ def SetupBuildEnv(env, prog_type, prog_name, source_files, install_dir):
         linux_redirect = ""
 
     source_objs = []
+    source_build_files = []
     for file in source_files:
+        build_env.VariantDir(
+            build_dir + "/" + os.path.dirname(file), os.path.dirname(file), duplicate=0)
+
+        file = build_dir + "/" + file
+        source_build_files.append(file)
 
         if(prog_type == 'shared'):
             build_obj = build_env.SharedObject(file,
-                                               SHCCCOM=build_env['SHCCCOM'] + " " + win_redirect + " > \"" + build_env['PROJECT_DIR'] + "/build/build_logs/" + os.path.splitext(
+                                               SHCCCOM=build_env['SHCCCOM'] + " " + win_redirect + " > \"" + build_env['PROJECT_DIR'] + "/" + build_dir + "/build_logs/" + os.path.splitext(
                                                    os.path.basename(file))[0] + "_compile.txt\" " + linux_redirect,
-                                               SHCXXCOM=build_env['SHCXXCOM'] + " " + win_redirect + " > \"" + build_env['PROJECT_DIR'] + "/build/build_logs/" + os.path.splitext(os.path.basename(file))[0] + "_compile.txt\" " + linux_redirect)
+                                               SHCXXCOM=build_env['SHCXXCOM'] + " " + win_redirect + " > \"" + build_env['PROJECT_DIR'] + "/" + build_dir + "/build_logs/" + os.path.splitext(os.path.basename(file))[0] + "_compile.txt\" " + linux_redirect)
             source_objs.append(build_obj)
         elif(prog_type == 'static' or prog_type == 'exec'):
 
             filename = os.path.splitext(os.path.basename(file))[0]
             build_obj = build_env.Object(file,
                                          CCCOM=build_env['CCCOM'] + " " + win_redirect + " > \"" + build_env['PROJECT_DIR'] +
-                                         "/build/build_logs/" + filename + "_compile.txt\" " + linux_redirect,
-                                         CXXCOM=build_env['CXXCOM'] + " " + win_redirect + " > \"" + build_env['PROJECT_DIR'] + "/build/build_logs/" + filename + "_compile.txt\" " + linux_redirect)
+                                         "/" + build_dir + "/build_logs/" + filename + "_compile.txt\" " + linux_redirect,
+                                         CXXCOM=build_env['CXXCOM'] + " " + win_redirect + " > \"" + build_env['PROJECT_DIR'] + "/" + build_dir + "/build_logs/" + filename + "_compile.txt\" " + linux_redirect)
             source_objs.append(build_obj)
+
+    progress.AddBuild(env, source_build_files, env.subst(
+        '$LIBPREFIX') + prog_name + env.subst('$LIBSUFFIX'))
 
     if(prog_type == 'shared'):
         if("Windows" in platform.system()):
@@ -279,7 +323,8 @@ def SetupBuildEnv(env, prog_type, prog_name, source_files, install_dir):
             for i in range(len(combinedActions)):
                 if(i == 0):
                     new_actions.append(Action.Action('${TEMPFILE("$SHLINK $SHLINKFLAGS $_SHLINK_TARGETS $_LIBDIRFLAGS $_LIBFLAGS $_PDB $_SHLINK_SOURCES", "$SHLINKCOMSTR")} 2>&1 > \"' +
-                                                     build_env['PROJECT_DIR'] + '/build/build_logs/' + prog_name + '_link.txt\"'))
+                                                     build_env['PROJECT_DIR'] + '/' + build_dir +
+                                                     '/build_logs/' + prog_name + '_link.txt\"'))
                 else:
                     new_actions.append(combinedActions[i])
             build_env['SHLINKCOM'] = new_actions
@@ -287,7 +332,7 @@ def SetupBuildEnv(env, prog_type, prog_name, source_files, install_dir):
             linkcom_string_match = re.sub(
                 r"\s\>\".*", "\",", build_env['SHLINKCOM'])
             build_env['SHLINKCOM'] = linkcom_string_match + str(
-                " > " + build_env['PROJECT_DIR'] + "/build/build_logs/" + prog_name + "_link.txt 2>&1")
+                " > " + build_env['PROJECT_DIR'] + "/" + build_dir + "/build_logs/" + prog_name + "_link.txt 2>&1")
     elif(prog_type == 'static' or prog_type == 'exec' or prog_type == 'unit'):
         if("Windows" in platform.system()):
             try:
@@ -298,7 +343,7 @@ def SetupBuildEnv(env, prog_type, prog_name, source_files, install_dir):
             for i in range(len(combinedActions)):
                 if(i == 0):
                     new_actions.append(Action.Action('${TEMPFILE("$LINK $LINKFLAGS /OUT:$TARGET.windows $_LIBDIRFLAGS $_LIBFLAGS $_PDB $SOURCES.windows", "$LINKCOMSTR")} 2>&1 > \"' +
-                                                     build_env['PROJECT_DIR'] + '/build/build_logs/' + prog_name + '_link.txt\"'))
+                                                     build_env['PROJECT_DIR'] + '/' + build_dir + '/build_logs/' + prog_name + '_link.txt\"'))
                 else:
                     new_actions.append(combinedActions[i])
 
@@ -308,7 +353,7 @@ def SetupBuildEnv(env, prog_type, prog_name, source_files, install_dir):
             linkcom_string_match = re.sub(
                 r"\s\>\".*", "\",", build_env['LINKCOM'])
             build_env['LINKCOM'] = linkcom_string_match + str(
-                " > " + build_env['PROJECT_DIR'] + "/build/build_logs/" + prog_name + "_link.txt 2>&1")
+                " > " + build_env['PROJECT_DIR'] + "/" + build_dir + "/build_logs/" + prog_name + "_link.txt 2>&1")
 
     if(prog_type == "shared"):
 
@@ -332,50 +377,56 @@ def SetupBuildEnv(env, prog_type, prog_name, source_files, install_dir):
                     if os.path.basename(os.path.splitext(str(node))[0]) == prog_name:
                         if("windows" in platform.system().lower()):
                             node.get_executor().set_action_list(Action.Action('${TEMPFILE("$CXX $_MSVC_OUTPUT_FLAG /c $CHANGED_SOURCES $CXXFLAGS $CCFLAGS $_CCCOMCOM","$CXXCOMSTR")} ' + win_redirect +
-                                                                              " > \"" + build_env['PROJECT_DIR'] + "/build/build_logs/" + prog_name + "_compile.txt\" " + linux_redirect, '$CXXCOMSTR'))
+                                                                              " > \"" + build_env['PROJECT_DIR'] + "/" + build_dir + "/build_logs/" + prog_name + "_compile.txt\" " + linux_redirect, '$CXXCOMSTR'))
                         else:
                             node.get_executor().set_action_list(Action.Action('$CXX -o $TARGET -c $CXXFLAGS $CCFLAGS $_CCCOMCOM $SOURCES ' + win_redirect +
-                                                                              " > \"" + build_env['PROJECT_DIR'] + "/build/build_logs/" + prog_name + "_compile.txt\" " + linux_redirect, '$CXXCOMSTR'))
+                                                                              " > \"" + build_env['PROJECT_DIR'] + "/" + build_dir + "/build_logs/" + prog_name + "_compile.txt\" " + linux_redirect, '$CXXCOMSTR'))
 
-    if not os.path.exists(build_env['PROJECT_DIR'] + "/build/build_logs"):
-        os.makedirs(build_env['PROJECT_DIR'] + "/build/build_logs")
+    if not os.path.exists(build_env['PROJECT_DIR'] + "/" + build_dir + "/build_logs"):
+        os.makedirs(build_env['PROJECT_DIR'] + "/" + build_dir + "/build_logs")
 
     # if ARGUMENTS.get('fail', 0):
     #    Command('target', 'source', ['/bin/false'])
 
     def print_cmd_line(s, targets, sources, env):
-        with open(env['PROJECT_DIR'] + "/build/build_logs/build_" + env['BUILD_LOG_TIME'] + ".log", "a") as f:
+        with open(env['PROJECT_DIR'] + "/" + build_dir + "/build_logs/build_" + env['BUILD_LOG_TIME'] + ".log", "a") as f:
             f.write(s + "\n")
 
     build_env['BUILD_LOG_TIME'] = datetime.datetime.fromtimestamp(
         time.time()).strftime('%Y_%m_%d__%H_%M_%S')
 
-    if(not GetOption('print_cmd')):
+    try:
+        print_cmd = GetOption('print_cmd')
+    except AttributeError:
+        print_cmd = False
+
+    if(not print_cmd):
         build_env['PRINT_CMD_LINE_FUNC'] = print_cmd_line
 
     built_bins = []
     if("Windows" in platform.system()):
         if(prog_type == 'shared'):
-            built_bins.append(build_env['BUILD_DIR'] + "/" + build_env.subst(
+            built_bins.append(build_dir + "/" + build_env.subst(
                 '$SHLIBPREFIX') + prog_name + build_env.subst('$SHLIBSUFFIX'))
         elif(prog_type == 'static'):
-            built_bins.append(build_env['BUILD_DIR'] + "/" + build_env.subst(
+            built_bins.append(build_dir + "/" + build_env.subst(
                 '$LIBPREFIX') + prog_name + build_env.subst('$LIBSUFFIX'))
         elif(prog_type == 'exec'):
-            built_bins.append(build_env['BUILD_DIR'] + "/" + build_env.subst(
+            built_bins.append(build_dir + "/" + build_env.subst(
                 '$PROGPREFIX') + prog_name + build_env.subst('$PROGSUFFIX'))
     else:
         if(prog_type == 'shared'):
-            built_bins.append(build_env['BUILD_DIR'] + "/" + build_env.subst(
+            built_bins.append(build_dir + "/" + build_env.subst(
                 '$SHLIBPREFIX') + prog_name + build_env.subst('$SHLIBSUFFIX'))
         elif(prog_type == 'static'):
-            built_bins.append(build_env['BUILD_DIR'] + "/" + build_env.subst(
+            built_bins.append(build_dir + "/" + build_env.subst(
                 '$LIBPREFIX') + prog_name + build_env.subst('$LIBSUFFIX'))
         elif(prog_type == 'exec'):
-            built_bins.append(build_env['BUILD_DIR'] + "/" + build_env.subst(
+            built_bins.append(build_dir + "/" + build_env.subst(
                 '$PROGPREFIX') + prog_name + build_env.subst('$PROGSUFFIX'))
 
     return [build_env, prog]
+
 
 def run_unit_tests(base_dir):
     """
